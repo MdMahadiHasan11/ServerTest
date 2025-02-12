cfe31c4bbb77d4a9e4ebcc06574534690ac93521
MdMahadiHasan11 <hasanpustcse11@gmail.com>
2025-02-12 23:44:31 +0600

    update

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+app.get((err,req,res,next) => { res.status(404).json({ message: 'Not Found' }); });
 app.get('/', (req, res) => {
-    res.send({ express: 'Hello From Express' });
+    res.status(200).json({ message: 'Hello From Express' });
   });
-app.listen(PORT,()=>{
+
+  app.listen(PORT,()=>{
     console.log(`Server is running on port AT http://localhost:${PORT}`);
 });
