e08302fabdbc415fcddf09e160dc4649ca5277a6
MdMahadiHasan11 <hasanpustcse11@gmail.com>
2025-02-13 00:18:40 +0600

    user create

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get((err,req,res,next) => { res.status(404).json({ message: 'Not Found' }); 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello From Express' });
   });
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
 
   app.listen(PORT,()=>{
     console.log(`Server is running on port AT http://localhost:${PORT}`);
